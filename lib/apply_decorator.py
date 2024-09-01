cc70c732cf9a502f5223b16f22ea3e54be65fb4a
plug_keith <keith.mwai@student.moringaschool.com>
2024-09-02 00:27:46 +0300

    Added example

--- lib/apply_decorator.py
+++ lib/apply_decorator.py
@@ -2,7 +2,15 @@ def decorator_func(func):
     def wrapper(*args, **kwargs):
         print('Decorator Applied')
         return func(*args, **kwargs)
-        return wrapper
-    
+    return wrapper  # This should be outside the wrapper function
+
 def apply_decorator(func):
     return decorator_func(func)
+
+@apply_decorator
+def say_hello(name):
+    """Function to greet someone."""
+    print(f'Hello, {name}!')
+
+# Call the decorated function
+say_hello('Keith')
