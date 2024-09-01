ffca439d34d979f376a60d6bf411c52b48e7889b
plug_keith <keith.mwai@student.moringaschool.com>
2024-09-02 00:14:13 +0300

    Added example

--- lib/reverse-string.py
+++ lib/reverse-string.py
@@ -1,2 +1,8 @@
 def reverse_string(text):
     return text[::-1]
+
+original_text = "Hello, World!"
+reversed_text = reverse_string(original_text)
+
+print("Original Text:", original_text)
+print("Reversed Text:", reversed_text)
