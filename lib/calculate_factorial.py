b346099877d50058b53a25b136837f646d845d51
plug_keith <keith.mwai@student.moringaschool.com>
2024-09-02 00:32:58 +0300

    Added example

--- lib/calculate_factorial.py
+++ lib/calculate_factorial.py
@@ -1,7 +1,12 @@
 def calculate_factorial(n):
+
     if n == 0:
-        return 1
+        return 1  # Factorial of 0 is 1
     factorial = 1
     for i in range(1, n + 1):
-        factorial *= i
-        return factorial
+        factorial *= i  # Multiply factorial by each number from 1 to n
+    return factorial 
+
+print(calculate_factorial(5))
+print(calculate_factorial(0))
+print(calculate_factorial(7)) 
